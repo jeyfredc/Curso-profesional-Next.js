5a64536bea9193e2f266a0ef5b25a12459610474
jeyfredc <jeyfredc@gmail.com>
2022-08-23 14:18:52 -0500

    Se realizan cambios hasta logout, pendiente hacer correcciones de bugs

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -39,8 +39,16 @@ function useProviderAuth() {
     }
   };
 
+  const logout = () => {
+    Cookie.remove('token');
+    setUser(null);
+    delete axios.defaults.headers.Authorization;
+    window.location.href = '/login';
+  };
+
   return {
     user,
     signIn,
+    logout,
   };
 }
