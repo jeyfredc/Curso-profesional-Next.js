516f43e76b228841e62456d4ba5b830d7adbfa72
jeyfredc <jeyfredc@gmail.com>
2022-08-23 14:24:25 -0500

    [UPDATE] Next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  eslint: {
+    // Con esta opcion podemos evitar que al ejecutar npm run build , nos arroje errores o sugerencias y poder subir un sitio a produccion, es recomendable no activarlo porque se puede estallar la pagina en producción pero es opcional para cada quien
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
